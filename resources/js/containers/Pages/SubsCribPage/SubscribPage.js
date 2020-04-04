b3657573a216b7f111e3cb185d421d915a326522
itech-corp <ibrahimmahamatmoussa@gmail.com>
2020-04-04 17:19:19 +0100

    from localhost to invest-calc/

--- resources/js/containers/Pages/SubsCribPage/SubscribPage.js
+++ resources/js/containers/Pages/SubsCribPage/SubscribPage.js
@@ -73,13 +73,13 @@ class SubscribPage extends Component {
                         <Col xs={9}>
                             <Row >
                                 <Col className="plan-1" xs={4}>
-                                    <a onClick={e=>this.clickHandler(e, 'http://127.0.0.1:8000/api/plans/gold-plan/payment')} href="/payement" ><img alt="plan" src={plan_1} /></a>
+                                    <a onClick={e=>this.clickHandler(e, 'invest-calc/api/plans/gold-plan/payment')} href="/payement" ><img alt="plan" src={plan_1} /></a>
                                 </Col>
                                 <Col xs={4}>
-                                    <a onClick={e=>this.clickHandler(e, 'http://127.0.0.1:8000/api/plans/diamond-plan/payment')}  href="/payement"><img alt="plan" src={plan_2} /></a>
+                                    <a onClick={e=>this.clickHandler(e, 'invest-calc/api/plans/diamond-plan/payment')}  href="/payement"><img alt="plan" src={plan_2} /></a>
                                 </Col>
                                 <Col className="mt-5" xs={4}>
-                                    <a onClick={e=>this.clickHandler(e, 'http://127.0.0.1:8000/api/plans/silver-plan/payment')} href="/payement"> <img alt="plan" src={plan_3} /></a>
+                                    <a onClick={e=>this.clickHandler(e, 'invest-calc/api/plans/silver-plan/payment')} href="/payement"> <img alt="plan" src={plan_3} /></a>
                                 </Col>
                             </Row>
                         </Col>
